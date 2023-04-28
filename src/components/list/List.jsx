b27ca26086e7786e04506c4bf3da53c24626cfa6
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-04-27 23:57:54 -0300

    implements fetchRepositories method

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from "react";
-import { ListGroup, Button, Spinner } from "react-bootstrap";
-import "./styles.css";
+import React, { useState } from 'react';
+import { ListGroup, Button, Spinner } from 'react-bootstrap';
+import './styles.css';
 
 const List = () => {
   const [repositories, setRepositories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchRepositories = async () => {
-    // TODO
+    setIsLoading(true);
+    const response = await fetch(
+      'https://api.github.com/users/devpass-tech/repos'
+    );
+    const data = await response.json();
+    setRepositories(data);
+    setIsLoading(false);
   };
 
   return (
-    <div className="list">
-      <div className="container">
-        <h2 className="title">Devpass Repositories</h2>
+    <div className='list'>
+      <div className='container'>
+        <h2 className='title'>Devpass Repositories</h2>
 
-        { isLoading ?
-        ( <Spinner/> ) : 
-        ( 
-          <ListGroup className="repositoriesList">
-
-          { /* TODO */ }
-          
-          </ListGroup> )}
-      <Button data-testid="button" className="button" variant="primary" onClick={() => fetchRepositories()}>Fetch repositories</Button>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ListGroup className='repositoriesList'>
+            {repositories.map((repo) => (
+              <ListGroup.Item key={repo.id}>{repo.name}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+        <Button
+          data-testid='button'
+          className='button'
+          variant='primary'
+          onClick={() => fetchRepositories()}
+        >
+          Fetch repositories
+        </Button>
       </div>
     </div>
   );
